e5c4684aeec7036a93c0f753a5e46eaf43b421df
unknown <brianna.albo@gmail.com>
2022-12-06 01:15:51 -0500

    adds notes to html, css, and js files

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,18 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// sets variables
 var currentDay = $("#currentDay")
 var clock = $("#clock");
 var hours = [$("#09"), $("#10"), $("#11"), $("#12"), $("#13"), $("#14"), $("#15"), $("#16"), $("#17")];
+// remove variable will be used to clear local storage
 var remove = $("#delete");
 var save = $(".saveBtn");
 
+// the current date is pulled from dayjs
+// this displays the current date in the header of the webpage
 currentDay.text(dayjs().format('MMMM-DD-YYYY'));
 $("#currentDay").css("font-size","32px");
 
+// the current time is pulled from dayjs
+// this displays the current time in the header of the webpage
 clock.text(dayjs().format('h:mm:a'));
 $("#clock").css("font-size", "26px");
 $("#clock").css("padding-left", "20px");
@@ -20,21 +23,26 @@ currentHour = dayjs().hour();
 
 console.log(currentHour);
 
+// this function loops through the hours array to color code each time block between 9am-5pm
 function timeBlock() {
   
   for (i = 0; i < hours.length; i++) {
     currentHour = dayjs().hour();
+    // sets the ids to the hour they represent in order to compare with the current time
     var hourNumber = hours[i].attr("id");
     console.log(hourNumber);
+// displays input the user saves in the specified time block
     hours[i].children("textarea").val(JSON.parse(localStorage.getItem(hourNumber)));
     
+    // future times will be green
     if (hourNumber > currentHour) {
       hours[i].children("textarea").css("background-color", "green");
     }
-   
+  //  past times will be turquoise
   else if (hourNumber < currentHour) {
     hours[i].children("textarea").css("background-color", "turquoise");
   }
+  // the present hour's timeblock will be displayed as red
   else if (hourNumber == currentHour) {
     hours[i].children("textarea").css("background-color", "red");
   }
@@ -42,46 +50,31 @@ function timeBlock() {
 }
 timeBlock();
 
+// when user enters input then clicks save button their input witll be saved to local storage
 $(save).on("click", function() {
   var userInput = $(this).siblings("textarea");
-  
+
   localStorage.setItem(userInput.parent().attr("id"), JSON.stringify(userInput.val()));
   
 })
 
-$ (remove).on("click", function() {
-  for (i = 0; i < hours.length; i++); 
-  var cb = document.querySelectorAll('.form-check-input');
-if (cb.checked == true) {
-  console.log('keep');
-}
-else if (cb.checked == false) {
-  var textArea = $(this).siblings("textarea");
-    textArea.val("");
-    localStorage.removeItem(textArea.parent().attr("id"));
-}
-})
+// The code below is a work in progress 
+// it will serve as a function to clear local storage in each time block using checkboxes and a 'clear button'
+// it is hidden from page to avoid user confusion and be worked on at a later date
+
+// $ (remove).on("click", function() {
+//   for (i = 0; i < hours.length; i++); 
+//   var cb = document.querySelectorAll('.form-check-input');
+// if (cb.checked == true) {
+//   console.log('keep');
+// }
+// else if (cb.checked == false) {
+//   var textArea = $(this).siblings("textarea");
+//     textArea.val("");
+//     localStorage.removeItem(textArea.parent().attr("id"));
+// }
+// })
 // var cb = document.querySelectorAll('.form-check-input');
         // console.log(cb.checked); // false
 
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
